6fd4bacd6d20d430db4303796c3f83d4c0f8d898
Raymond <r.wong@live.com>
2015-02-27 21:37:44 -0500

    init, str, add tags in task

--- task.py
+++ task.py
@@ -1,23 +1,22 @@
-#This is the code taskmgr.py code
 class task:
-    def __init__(self,tid):
-
+    def __init__(self, tid, desc, priority, tags):
         self.tid = tid
-        self.name = raw_input('enter a task: ')
-        self.tags = []
-        self.priority = False
-        
-    def add_tags(self):
-        phrase = self.name
-        print "%d tag(s) found" % phrase.count('#')
-        for x in phrase.split():
-            if x[0] == '#':
-                self.tags.append(x)
-    
+        self.desc = desc
+        self.priority = priority
+        self.tags = tags.split()
+
+    def __str__(self):
+        return """tid: {}
+desc: {}
+priority: {}
+tags: {}""".format(self.tid,
+                       self.desc,
+                       self.priority,
+                       self.tags)
+
+    def add_tags(self, tags):
+        for index, tag in enumerate(tags.split()):
+            self.tags.append(tag)
 
     def check_priority(self):
-        if self.name[0] == '!':
-            print 'This task is a Priority!'
-            self.priority = True
-        else:
-            self.priority = False
+        pass
