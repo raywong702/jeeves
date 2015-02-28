345fad6600738d62d1da6ed279ffc5ab4aacca92
Raymond <r.wong@live.com>
2015-02-27 22:11:47 -0500

    fixed tag classes

--- tag.py
+++ tag.py
@@ -3,3 +3,6 @@ class tag:
     def __init__(self, tag, priority = 0):
         self.tag = tag
         self.priority = priority
+
+    def __str__(self):
+        return "{} : {}".format(self.tag, self.priority)
--- tag_list.py
+++ tag_list.py
@@ -4,5 +4,14 @@ class tag_list:
 
     def __init__(self, tag = None):
         self.tag_list = []
+        self.append(tag)
+
+    def __str__(self):
+        ret = ""
+        for index, tag in enumerate(self.tag_list):
+            ret += str(tag) + "\n"
+        return ret
+
+    def append(self, tag = None):
         if tag != None:
             self.tag_list.append(tag)
